52d9c98f6a0f1dd5953e6db1bef9c44b720d1989
SantiiMari <44756162+SantiiMari@users.noreply.github.com>
2019-05-04 05:25:13 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
       <Nav />
       <Switch>
       <Route exact path="/" component={Search}/>
+    <Route component={NoMatch} />
       <Route exact path="/saved" component={Saved}/>
       
       {/* <InputField></InputField> */}
