bdaeb39f18201772a97e7c60595366bc26225686
SantiiMari <44756162+SantiiMari@users.noreply.github.com>
2019-05-04 05:23:15 -0500

    Update Book.js

--- src/models/Book.js
+++ src/models/Book.js
@@ -1,17 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const Book = mongoose.model("Book, BookSchema");
+
 
 const BookSchema = new Schema({
     title: String,
-    authors: [String],
+    authors: String,
     description: String,
-    img: {
-        type: {String},
-    },
-    link: {
-        type: String,
-    }
+    link: String,
+    
 });
 
+
+const Book = mongoose.model("Book, BookSchema");
 module.exports = Book;
