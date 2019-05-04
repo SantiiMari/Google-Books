9a1189c23b38f6785561f116af38a9b4814c9e06
SantiiMari <44756162+SantiiMari@users.noreply.github.com>
2019-05-04 05:34:17 -0500

    Add files via upload

---
+++ src/Utils/API.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const URL = "https://www.googleapis.com/books/v1/volumes?q=";
+
+export default {
+  searchBooks: function(query) {
+    return axios.get(URL + query);
+  },
+  getBooks: function() {
+    return axios("/api/books");
+  },
+  addBooktoDB: function(bookData) {
+    return axios.post("/api/books", bookData);
+  },
+  deleteBook: function(id) {
+    return axios.delete("api/books/" + id)
+  }
+};
